2050857bc3424bcc84be58080692cec74aed764e
riya-chauhan12 <riyachauhan1853@gmail.com>
2026-09-26 21:40:51 +0530

    Add knowledge loading, embedding generation, and Qdrant upload functionality

--- basicRAG/VectorRag.py
+++ basicRAG/VectorRag.py
@@ -39,3 +39,66 @@ print(f"Vector size:{EMBEDDING_SIZE}")
 print("Distance :Cosine")
 
 #load our knowledge
+with open("knowledge.txt","r",encoding="utf-8") as f:
+    documents=[
+        line.strip()
+        for line in f:
+        if line.strin()
+    ]
+    #["line1","line2"....]
+print(f"Loaded {len(documents)} documents")
+
+#create embeddigns
+print("loading embedding model....")
+model=SenteceTransformer("all-MiniLM-L6-v2")
+
+print("Embedding model ready!")
+
+embeddings=model.encode(documents)
+print(f"Generated {len(embeddings)} embeddings")
+print(f"Embeddigns size: {len(embeddings[0])} ")
+
+
+#qdrant point
+
+points=[]
+for i,embeddings in enumerate(embeddings):
+    point=PointStruct(
+        id=i+1,
+        vector=embedding.tolist(),
+        payload={
+            "text":documents[i]
+        }
+    )
+    points.append(point)
+
+#upload to qdrant
+client.upsert(
+    collection_name=COLLECTION_NAME,
+    points=points
+)
+print(f"uploaded {len(points)} document to qdrant")
+
+#search qdrant
+def search(query, top_k=3):
+    #conver the question to embedding
+    query_vector=model.encode(query).tolist()
+    #search in qdrant
+    results=client.query_points(
+        collection_name=COLLECTION_NAME,
+        query=query_vector,
+        limit=top_k,
+        with_payload=True,
+    ).points
+    return results
+
+#test search
+quey="how many vaction days i can get?"
+results=serach(query,top_k=3)
+print("\n Search Results:")
+for result in results:
+    print(f"Score:{result.score:.3f}")
+    print(result.payload["text"])
+    print()
+
+
